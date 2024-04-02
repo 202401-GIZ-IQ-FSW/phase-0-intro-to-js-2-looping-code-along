7373e0a01b4bd42acd28a14f755ef5ee0d00da05
Hanan-02 <151002956+Hanan-02@users.noreply.github.com>
2024-04-02 12:47:09 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function writeCards(names, event) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
 // Code your solutions in this file
